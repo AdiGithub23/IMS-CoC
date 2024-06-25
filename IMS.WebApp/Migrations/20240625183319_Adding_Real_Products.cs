30927e6c3cff7d27e464e3a14dcd66b72dec45bb
AdiGithub23 <gunathilakeadheesha23@gmail.com>
2024-06-26 00:28:27 +0530

    AddProduct_button_position_changed

---
+++ IMS.WebApp/Migrations/20240625183319_Adding_Real_Products.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMS.WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adding_Real_Products : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
