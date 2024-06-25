4fd3e4ecc731524f2bad66eb360ff25afbf0b6ed
Chathura Dissanayake <chathurakosala6@gmail.com>
2024-06-26 03:53:35 +0530

    update branch

---
+++ IMS.WebApp/Migrations/20240625131850_initial_migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMS.WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
